90dc04cfdace0f710ef4d3cea9a98b8d6251c8a7
Jason Leach <jlspursfan@gmail.com>
2019-06-04 23:05:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort.reverse!
+end
+
+def sort_array_char_count(arr)
+  arr.sort_by { |ele| ele.length }
+end
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  return arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
